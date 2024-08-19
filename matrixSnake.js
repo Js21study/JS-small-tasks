2e664f03afdada3cfe87400e5a8e70cbec1aa05a
student@student.com <ti@admin.com>
2024-08-19 23:02:56 +0300

    new task matrix snake

--- matrixSnake.js
+++ matrixSnake.js
@@ -1,3 +1,11 @@
+// NOTE: There are need to be in output matrix snake. Example output:
+// [
+//     1,  2,  3,  4,  5, 10, 15, 20,
+//    25, 24, 23, 22, 21, 16, 11,  6,
+//     7,  8,  9, 14, 19, 18, 17, 12,
+//    13
+//]
+
 let res = [];
 
 const snakeLogic = (anyArr, dir, m) => {
